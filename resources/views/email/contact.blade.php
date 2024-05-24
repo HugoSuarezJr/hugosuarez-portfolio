347216ef875f3ed9d41ff83b77a106cae82e30da
Hugo Suarez <hugo.suarez92@yahoo.com>
2024-05-24 17:49:44 -0400

    updated contact form html

--- resources/views/email/contact.blade.php
+++ resources/views/email/contact.blade.php
@@ -1,14 +1,16 @@
 <h3>You have a contact request!</h3>
 <table>
     <tr>
-        <th>Name</th>
+        <th>Name:</th>
         <td>{{ $name }}</td>
     </tr>
     <tr>
-        <th>Email</th>
-        <td>{{ $email }}</td>
+        <th>Email:</th>
+        <td><a href="mailto:{{ $email }}" target="_blank">{{ $email }}</a></td>
     </tr>
 </table>
 
-
-<div>{{ $body }}</div>
+<div>
+    <h4 style="text-decoration-line: underline; margin-bottom: 2px;">Message:</h4>
+    <p style="font-size: 20px; font-style: italic; margin-top: 0px;">{{ $body }}</p>
+</div>
