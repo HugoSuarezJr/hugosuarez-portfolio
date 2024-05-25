420f3bbaaae1ccb5039a2d059b5968d5e77374f8
Hugo Suarez <hugo.suarez92@yahoo.com>
2024-05-25 19:48:02 -0400

    updated composer versions

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Hugo's Portfolio</title>
+    <title>Hugo Suárez</title>
 
     <meta name="csrf-token" content="{{csrf_token()}}">
 
