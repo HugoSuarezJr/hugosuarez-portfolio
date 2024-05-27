f665d8538ff1d9185a4fdacb66aa62de6a2fff3b
Hugo Suarez <hugo.suarez92@yahoo.com>
2024-05-27 19:12:44 -0400

    update favicon variations

--- resources/views/components/favicon.blade.php
+++ resources/views/components/favicon.blade.php
@@ -1,4 +1,9 @@
-<link rel="apple-touch-icon" sizes="180x180" href="/img/favicon_io/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon_io/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon_io/favicon-16x16.png">
-<link rel="manifest" href="/img/favicon_io/site.webmanifest">
+<link rel="apple-touch-icon" sizes="180x180" href="/img/favicon.ico/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon.ico/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon.ico/favicon-16x16.png">
+<link rel="manifest" href="/img/favicon.ico/site.webmanifest">
+<link rel="mask-icon" href="/img/favicon.ico/safari-pinned-tab.svg" color="#0f172a">
+<link rel="shortcut icon" href="/img/favicon.ico/favicon.ico">
+<meta name="msapplication-TileColor" content="#0f172a">
+<meta name="msapplication-config" content="/img/favicon.ico/browserconfig.xml">
+<meta name="theme-color" content="#0f172a">
